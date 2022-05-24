339107164f7faddabb77a281f3f9a2c23b994baf
shobana <shobanag@texe.com>
2022-05-24 12:54:01 +0530

    Testing 2

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,10 @@
 import { HighlightDirective } from './highlight.directive';
-
+import { ElementRef, Renderer2 } from '@angular/core';
 describe('HighlightDirective', () => {
+  let el: ElementRef, renderer: Renderer2
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+
+    const directive = new HighlightDirective(el, renderer);
     expect(directive).toBeTruthy();
   });
 });
